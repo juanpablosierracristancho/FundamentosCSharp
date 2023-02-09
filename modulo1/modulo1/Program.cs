888d9090fb5d1d57cbc7f2d7172dc8e8038fc2f6
juanpablosierracristancho <124634727+juanpablosierracristancho@users.noreply.github.com>
2023-02-08 19:34:41 -0500

    INICIANDO
    APRENDIENDO

---
+++ modulo1/modulo1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace modulo1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        
+        Console.WriteLine("ingresa tu nombre");
+        string usuario = Console.ReadLine();
+        Console.WriteLine("El nombre del usuario es "+usuario);
+    }
+}
+

8af2a9a12c6ac0b21027d7d4aac31d20def4faa2
juanpablosierracristancho <124634727+juanpablosierracristancho@users.noreply.github.com>
2023-02-08 19:39:25 -0500

    INICIANDO
    APRENDIENDO

---
+++ modulo2/modulo2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace modulo2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int numeroSuperGrande = int.MaxValue;
+        Console.WriteLine(numeroSuperGrande);
+    }
+}
+
